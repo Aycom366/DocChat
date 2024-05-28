d9712452de798216a4ffb185250c1555afd8d711
aycom366 <bamigboyeayomide200@gmail.com>
2024-05-28 11:43:31 +0100

    added auth linking

--- components/shared/Container.tsx
+++ components/shared/Container.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { cn } from "@/lib/utils";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { ElementType, ReactNode } from "react";
 
 export const Container = ({
@@ -10,14 +13,32 @@ export const Container = ({
   as?: ElementType;
   className?: HTMLDivElement["className"];
 }) => {
+  const pathname = usePathname();
+
+  console.log(pathname);
   return (
     <Component
       className={cn(
-        "mx-auto w-full max-w-screen-xl px-2.5 md:px-20",
+        "mx-auto flex flex-col gap-4 w-full max-w-screen-xl px-2.5 md:px-20",
         className
       )}
     >
       {children}
+      {pathname === "/auth/login" ? (
+        <div className='flex flex-row text-sm items-center gap-1'>
+          <p>Don&apos;t have an account?</p>
+          <Link className='underline font-medium' href='/auth/register'>
+            Register
+          </Link>
+        </div>
+      ) : (
+        <div className='flex text-sm flex-row items-center gap-1'>
+          <p>Have an account?</p>
+          <Link className='underline font-medium' href='/auth/login'>
+            Log In
+          </Link>
+        </div>
+      )}
     </Component>
   );
 };
