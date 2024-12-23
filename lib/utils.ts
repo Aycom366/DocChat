7f3c9a09112e8dfdadc19a27fd0754c07fad6af7
aycom366 <bamigboyeayomide200@gmail.com>
2024-12-23 23:44:36 +0100

    updated stripe redirect

--- lib/utils.ts
+++ lib/utils.ts
@@ -13,7 +13,8 @@ export function cn(...inputs: ClassValue[]) {
 
 export function getAbsoluteUrl(path: string) {
   if (typeof window !== "undefined") return path;
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
+  if (process.env.NEXT_PUBLIC_PROD_URL)
+    return `https://${process.env.NEXT_PUBLIC_PROD_URL}${path}`;
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
 }
 
